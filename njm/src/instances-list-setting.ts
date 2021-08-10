38a569b4482935e38dcd0b9f321bb2af5d19614d
tamaina <tamaina@hotmail.co.jp>
2021-08-10 15:54:37 +0900

    wip

--- njm/src/instances-list-setting.ts
+++ njm/src/instances-list-setting.ts
@@ -1,12 +1,23 @@
+import { lang } from "./locale";
+
+export const repositories = ['syuilo/misskey', 'mei23/misskey'] as const;
+export const instanceLanguages = ['ja', 'en', 'de', 'fr', 'zh', 'ko', 'ru', 'de', 'th', 'es'] as const;
+
+const languageLookup: { [x: string]: typeof instanceLanguages[number][] } = {
+    'ja-JP': ['ja'],
+    'en-US': ['en', 'de', 'fr', 'zh', 'ko', 'ru', 'de', 'th', 'es'],
+};
+
 export interface InstancesSetting {
-    repository: ('syuilo/misskey' | 'mei23/misskey')[];
+    repository: typeof repositories[number][];
     sortCriteria: 'default' | 'version';
     sortDescendingOrder: boolean;
-
+    language: typeof instanceLanguages[number][];
 };
 
 export const defaultInstancesSetting: InstancesSetting = {
-    repository: ['syuilo/misskey', 'mei23/misskey'],
+    repository: [...repositories],
     sortCriteria: 'default',
     sortDescendingOrder: true,
+    language: languageLookup[lang] || [...instanceLanguages],
 };
