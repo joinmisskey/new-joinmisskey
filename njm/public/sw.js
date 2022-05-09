f67be4162d566069a80ebc46eb3355dc3ada80fe
tamaina <tamaina@hotmail.co.jp>
2022-05-10 03:53:33 +0900

    fix

--- njm/public/sw.js
+++ njm/public/sw.js
@@ -4,7 +4,7 @@ self.addEventListener('fetch', ev => {
 		fetch(ev.request)
 		.catch(e => {
 			console.error(e, ev.request);
-			return new Response(`Offline. Service Worker @${version}\n${e}`, { status: 200, statusText: 'NG SW' });
+			return new Response(`Offline. joinmisskey Service Worker\n${e}`, { status: 200, statusText: 'NG SW' });
 		})
 	);
 });
