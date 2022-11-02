66dbc83967d56ac7c7fa80d918390e59d7b01f85
tamaina <tamaina@hotmail.co.jp>
2022-11-03 01:43:18 +0900

    add cs, id

--- njm/src/instances-list-setting.ts
+++ njm/src/instances-list-setting.ts
@@ -4,11 +4,11 @@ import { lang } from "./locale";
 export const repositories = ['syuilo/misskey', 'mei23/misskey', 'TeamBlackCrystal/misskey', 'mei23/misskey-v11', 'Groundpolis/Groundpolis', 'kokonect-link/cherrypick'] as const;
 export const orderOptions = ['default', 'originalNotesCount', 'originalUsersCount', 'reactionsCount'] as const;
 export const registrationStatuses = ['open', 'close'] as const;
-export const instanceLanguages = ['ja', 'en', 'de', 'fr', 'zh', 'ko', 'ru', 'th', 'es', 'nl', 'bh', 'sv', 'it'] as const;
+export const instanceLanguages = ['ja', 'en', 'de', 'fr', 'zh', 'ko', 'ru', 'th', 'es', 'nl', 'bh', 'sv', 'it', 'cs', 'id'] as const;
 
 export const languageLookup: { [x: string]: typeof instanceLanguages[number][] } = {
     'ja-JP': ['ja'],
-    'en-US': ['en', 'fr', 'th', 'es', 'nl', 'bh', 'sv', 'it'],
+    'en-US': ['en', 'fr', 'th', 'es', 'nl', 'bh', 'sv', 'it', 'cs', 'id'],
     'de-DE': ['de'],
     'ko-KR': ['ko'],
     'zh-TW': ['zh'],
