24a7411761c579cd39c96595828c7823001af897
tamaina <tamaina@hotmail.co.jp>
2022-11-18 00:08:49 +0900

    :v:

--- njm/src/App.vue
+++ njm/src/App.vue
@@ -2,13 +2,11 @@
   <div id="app">
     <Splash :show="$store.state.splash" />
     <router-view/>
-    <!--<Features/>-->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-// import Features from '@/components/features.vue';
 import Splash from '@/components/splash.vue';
 import { lang } from '@/locale';
 import { injectLang } from '@/inject-lang';
@@ -16,7 +14,6 @@ import { injectLang } from '@/inject-lang';
 export default defineComponent({
   components: {
     Splash,
-    // Features
   },
 
   data() {
--- njm/src/features.ts
+++
@@ -1,8 +0,0 @@
-// Featureのidを定義しつつ、画像パスを定義します
-
-export const features: [string, (null | string)][] = [
-    ["mfm", null],
-    ["reactions", null],
-    ["visibility", "2021/03/visibility.png"],
-
-];
